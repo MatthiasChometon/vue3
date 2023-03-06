e0a4a14659e4645a40c5a64041ba03a84e9ed2e8
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-06 11:41:46 +0100

    refactor: remove apollo

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,18 @@
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { DefaultApolloClient } from '@vue/apollo-composable'
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
-import { setContext } from '@apollo/client/link/context'
-import { createUploadLink } from 'apollo-upload-client'
 import { Dialog, Notify, Quasar } from 'quasar'
 import App from './App.vue'
 
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
-import 'uno.css'
 
 const routes = setupLayouts(generatedRoutes)
-const cache = new InMemoryCache({
-  addTypename: true,
-})
-const uri = `${import.meta.env.VITE_GRAPHQL_URL}/graphql`
-
-const link = createUploadLink({
-  uri,
-})
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('accessToken')
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  }
-})
-
-const apolloClient = new ApolloClient({
-  link: authLink.concat(link),
-  cache,
-  uri,
-  defaultOptions: { mutate: { errorPolicy: 'all' } },
-})
 
 export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
-    ctx.app.provide(DefaultApolloClient, apolloClient)
     ctx.app.use(Quasar, {
       plugins: { Notify, Dialog },
     })
